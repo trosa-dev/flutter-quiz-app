3776dc3ce2ef786b14afa88bb61304e6e174c4ad
TRosa Dev <thiagosrosa@gmail.com>
2023-11-07 08:17:15 -0300

    feat: add buttons to questions

--- lib/app/screens/questions/index.dart
+++ lib/app/screens/questions/index.dart
@@ -14,8 +14,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('QuestionsView'),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
     );
   }
 }
